d9ec5b43caefa71b76c1136c736e7275754abe6e
Catisinha <luacoutinhor@gmail.com>
2023-04-10 10:50:21 -0300

    Modificação do tipo.cs

---
+++ Pokedex/Models/tipo.cs
@@ -0,0 +1,10 @@
+namespace Pokedex.Models
+{
+    public class tipo
+    {
+        //Atributos da Classe Tipo - Caracteristicas dos Tipos
+        //baseado no arquivo tipos.json
+       public string Nome { get; set; } 
+       public string Cor { get; set; }
+    }
+}
